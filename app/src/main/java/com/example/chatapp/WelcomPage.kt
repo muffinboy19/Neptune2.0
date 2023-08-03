4ff9b4be8e4c75e0668b1e1caef677623db8d7e3
muffinboy19 <gauravchhetri6363@gmail.com>
2023-08-03 21:59:32 +0530

    intial commit does not contain any thing

---
+++ app/src/main/java/com/example/chatapp/WelcomPage.kt
@@ -0,0 +1,15 @@
+
+package com.example.chatapp
+/*
+this page would contain regitser and lgoin links
+ */
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcom_page)
+    }
+}
