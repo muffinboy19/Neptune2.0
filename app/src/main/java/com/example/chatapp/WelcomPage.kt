815b3f3952c35a6c0e09a09ef493ceb79aeb75e4
ishant <ishantkuwt@gmail.com>
2023-08-04 01:28:29 +0530

    commit 1 by Ishant welcome page UI

--- app/src/main/java/com/example/chatapp/WelcomPage.kt
+++ app/src/main/java/com/example/chatapp/WelcomPage.kt
@@ -6,10 +6,14 @@ this page would contain regitser and lgoin links
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class WelcomPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcom_page)
+
+        val agreeAndContinueButton = findViewById<AppCompatButton>(R.id.agreeAndContinueButton)
+
     }
 }
